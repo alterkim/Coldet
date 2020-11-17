0f1f7fe79a5cc593c8e5c0a314d2f0f0c46395e8
alterkim <alter@kakao.com>
2020-11-18 01:25:04 +0900

    First commit for MainActivity

---
+++ Coldet/src/main/java/com/coldblock/coldet/Coldet.java
@@ -0,0 +1,15 @@
+package com.coldblock.coldet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class Coldet extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        
+    }
+}
