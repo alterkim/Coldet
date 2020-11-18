d502c2183bf88309ecff75d7bd65e2577682b0cf
alterkim <alter@kakao.com>
2020-11-18 15:02:15 +0900

    Add activity for adding wallet

--- Coldet/src/main/java/com/coldblock/coldet/Coldet.java
+++ Coldet/src/main/java/com/coldblock/coldet/Coldet.java
@@ -1,8 +1,13 @@
 package com.coldblock.coldet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.coldblock.coldet.wallet.AddWalletActivity;
 
 public class Coldet extends Activity {
     @Override
@@ -10,6 +15,14 @@ public class Coldet extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        
+
+        ImageButton add_wallet = findViewById(R.id.btn_add_wallet);
+        add_wallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Coldet.this, AddWalletActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Coldet/src/main/java/com/coldblock/coldet/wallet/AddWalletActivity.java
@@ -0,0 +1,18 @@
+package com.coldblock.coldet.wallet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.coldblock.coldet.R;
+
+public class AddWalletActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_wallet);
+
+
+    }
+}
