ee60aa0e90c5544302dc5d9ae85414365fb0e3f9
alterkim <alter@kakao.com>
2020-11-22 02:25:55 +0900

    Move NFC detect from NfcActivity

--- Coldet/src/main/java/com/coldblock/coldet/nfc/dialog/TagNfcDialog.java
+++ Coldet/src/main/java/com/coldblock/coldet/nfc/dialog/TagNfcDialog.java
@@ -2,8 +2,12 @@ package com.coldblock.coldet.nfc.dialog;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -18,9 +22,13 @@ import com.google.android.gms.common.api.CommonStatusCodes;
 public class TagNfcDialog extends Activity implements View.OnClickListener {
 
     public static final int REQUEST_NFC_TRANSMISSION = 1020;
+    public static final String NFC_ADAPTER = "nfcAdapter";
 
     private byte[] serializedTransaction;
     private TagNfcDialogListener tagNfcDialogListener;
+
+    private NfcAdapter nfcAdapter;
+    private PendingIntent pendingIntent;
 
 
     public void setDialogListener(TagNfcDialogListener tagNfcDialogListener) {
@@ -45,9 +53,9 @@ public class TagNfcDialog extends Activity implements View.OnClickListener {
         Button button = findViewById(R.id.btn_nfc_confirm);
         button.setOnClickListener(this);
 
-        Intent data = new Intent(TagNfcDialog.this, NfcActivity.class);
-        data.putExtra(WalletAddedActivity.Transaction, serializedTransaction);
-        startActivityForResult(data, REQUEST_NFC_TRANSMISSION);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        Intent nfcIntent = new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        pendingIntent = PendingIntent.getActivity(this, 0, nfcIntent, 0);
     }
 
     @Override
@@ -69,5 +77,50 @@ public class TagNfcDialog extends Activity implements View.OnClickListener {
                 }
             }
         }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if (nfcAdapter != null) {
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        if (tag != null) {
+            byte[] tagId = tag.getId();
+            System.out.println("TagID: " + toHexString(tagId));
+
+            ProcessNFC();
+        }
+    }
+
+    private void ProcessNFC(){
+        Intent data = new Intent(TagNfcDialog.this, NfcActivity.class);
+        data.putExtra(WalletAddedActivity.Transaction, serializedTransaction);
+        startActivityForResult(data, REQUEST_NFC_TRANSMISSION);
+    }
+
+    public static final String CHARS = "0123456789ABCDEF";
+
+    public static String toHexString(byte[] data) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < data.length; ++i) {
+            sb.append(CHARS.charAt((data[i] >> 4) & 0x0F))
+                    .append(CHARS.charAt(data[i] & 0x0F));
+        }
+        return sb.toString();
     }
 }
