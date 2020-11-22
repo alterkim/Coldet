6c63a8877a2953bae1ed74a4b2eb3532b23354cc
alterkim <alter@kakao.com>
2020-11-22 16:47:50 +0900

    Make NFC read function & Delete confirm button

--- Coldet/src/main/java/com/coldblock/coldet/nfc/dialog/TagNfcDialog.java
+++ Coldet/src/main/java/com/coldblock/coldet/nfc/dialog/TagNfcDialog.java
@@ -5,40 +5,46 @@ import android.app.Dialog;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.Ndef;
 import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.coldblock.coldet.R;
 import com.coldblock.coldet.nfc.activity.NfcActivity;
 import com.coldblock.coldet.wallet.activity.WalletAddedActivity;
 import com.google.android.gms.common.api.CommonStatusCodes;
 
-public class TagNfcDialog extends Activity implements View.OnClickListener {
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class TagNfcDialog extends Activity {
     private static final String TAG = "TagNfcDialog";
 
     public static final int REQUEST_NFC_TRANSMISSION = 1020;
     public static final String NFC_ADAPTER = "nfcAdapter";
 
     private byte[] serializedTransaction;
-    private TagNfcDialogListener tagNfcDialogListener;
 
     private NfcAdapter nfcAdapter;
     private PendingIntent pendingIntent;
-
-
-    public void setDialogListener(TagNfcDialogListener tagNfcDialogListener) {
-        this.tagNfcDialogListener = tagNfcDialogListener;
-    }
-
+    private IntentFilter[] mFilters;
+    private String [][] mTechLists;
 
     @Override
-    protected void onCreate(Bundle savedBundle){
+    protected void onCreate(Bundle savedBundle) {
         super.onCreate(savedBundle);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_tag_nfc);
@@ -51,25 +57,71 @@ public class TagNfcDialog extends Activity implements View.OnClickListener {
         Intent intent = getIntent();
         serializedTransaction = intent.getByteArrayExtra(WalletAddedActivity.Transaction);
 
-        Button button = findViewById(R.id.btn_nfc_confirm);
-        button.setOnClickListener(this);
-
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         Intent nfcIntent = new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         pendingIntent = PendingIntent.getActivity(this, 0, nfcIntent, 0);
+
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+
+        try {
+            ndef.addDataType("*/*");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+
+        mFilters = new IntentFilter[]{
+                ndef,
+        };
+
+        // Setup a tech list for all NFC tags
+        mTechLists = new String[][]{
+                new String[]{MifareClassic.class.getName()}
+        };
+
     }
 
-    @Override
-    public void onClick(View v) {
-//        if (v.getId() == R.id.btn_nfc_confirm) {
-//            String test = "Done";
-//            tagNfcDialogListener.getSignedTransaction(test);
-//        }
+    private void resolveIntent(Intent intent) {
+        // Parse the intent and get the action that triggered this intent
+        String action = intent.getAction();
+        ReadNFC(intent, action);
     }
+
+    private void ReadNFC(Intent intent, String action) {
+        // Check if it was triggered by a tag discovered interruption
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+            // Get an instance fo the TAG from the NfcAdapter
+            Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            // Get an instance of the Mifare classic card from this TAG intent
+            MifareClassic tagMfc = MifareClassic.get(tagFromIntent);
+
+            byte[] data;
+            try {
+                tagMfc.connect();
+
+                for (int i = 0; i < tagMfc.getSectorCount(); i++ ){
+                    if(tagMfc.authenticateSectorWithKeyA(i, MifareClassic.KEY_DEFAULT)) {
+                        Log.d(TAG, "Authentication success!");
+
+                        for(int j = 0; j < tagMfc.getBlockCountInSector(j); j++) {
+                            data = tagMfc.readBlock(4*i +j);
+                            Log.d(TAG, toHexString(data));
+                        }
+                    }
+                    else {
+                        Log.d(TAG, "Authentication failed!");
+                    }
+
+                }
+            } catch (IOException e) {
+                Log.e(TAG, e.getLocalizedMessage());
+            }
+        }
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQUEST_NFC_TRANSMISSION) {
             if (resultCode == CommonStatusCodes.SUCCESS) {
@@ -81,10 +133,10 @@ public class TagNfcDialog extends Activity implements View.OnClickListener {
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         if (nfcAdapter != null) {
-            nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, mFilters, mTechLists);
         }
     }
 
@@ -99,19 +151,21 @@ public class TagNfcDialog extends Activity implements View.OnClickListener {
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-        if (tag != null) {
-            byte[] tagId = tag.getId();
-            System.out.println("TagID: " + toHexString(tagId));
-
-            ProcessNFC();
-        }
+        resolveIntent(intent);
+//        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+//        if (tag != null) {
+//            ProcessNFC();
+//        }
     }
 
     private void ProcessNFC(){
         Intent data = new Intent(TagNfcDialog.this, NfcActivity.class);
         data.putExtra(WalletAddedActivity.Transaction, serializedTransaction);
         startActivityForResult(data, REQUEST_NFC_TRANSMISSION);
+    }
+    
+    private void WriteTransactionToTag() {
+        //TODO: Write the transaction data to tag
     }
 
     public static final String CHARS = "0123456789ABCDEF";
@@ -123,5 +177,17 @@ public class TagNfcDialog extends Activity implements View.OnClickListener {
                     .append(CHARS.charAt(data[i] & 0x0F));
         }
         return sb.toString();
+    }
+
+    public static String byteArrayToHexString(byte[] data){
+        int length = data.length;
+        StringBuilder string_data = new StringBuilder();
+
+        for (byte datum : data) {
+            string_data.append(Integer.toHexString((datum >> 4) & 0xf));
+            string_data.append(Integer.toHexString(datum & 0xf));
+        }
+
+        return string_data.toString();
     }
 }
