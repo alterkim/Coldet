5c060f64d0b12d2bb66e2b073f76aa98186f60a3
alterkim <alter@kakao.com>
2020-12-04 12:07:32 +0900

    Add network data

---
+++ Coldet/src/main/java/com/coldblock/coldet/network/Data.java
@@ -0,0 +1,8 @@
+package com.coldblock.coldet.network;
+
+public class Data {
+    public static final String YEOUIDO_TESTNET = "https://bicon.net.solidwallet.io";
+    public static final String MAINNET = "https://ctz.solidwallet.io";
+    public static final String EULJIRO_TESTNET = "https://test-ctz.solidwallet.io";
+    public static final String PAGODA_TESTNET = "https://zicon.net.solidwallet.io";
+}
